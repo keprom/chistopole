a6320e87b06730f8447dc266817f92895b305d00
keprom <kokshetau.energo.prom@mail.ru>
2018-11-19 15:12:35 +0600

    nice tray #3

--- system/application/views/reports/schetoplata.php
+++ system/application/views/reports/schetoplata.php
@@ -19,158 +19,133 @@ function f_d2($var)
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="ru">
 <head>
     <title>Счет на оплату</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
     <link rel="stylesheet" href="/css/fullpage.css">
 </head>
 <body class="portrait">
-<table width="100%">
+<?php $data_schet = strlen($data_schet) == 0 ? datetostring($schetfactura_date->date) : $data_schet; ?>
+<table class="block">
     <tr>
-        <td align="left">
-            <center><b>СЧЕТ<?php echo $schet; ?> №</b>
-                <?php echo $number . ' от ';
-                if (strlen($data_schet) == 0) {
-                    echo datetostring($schetfactura_date->date);
-                } else {
-                    echo $data_schet;
-                } ?>
-            </center>
-        </td>
+        <td align="center"><b>СЧЕТ<?php echo $schet; ?></b> №<?php echo $number . ' от ' . $data_schet; ?></td>
         <td> (1)</td>
     </tr>
-
     <tr>
-        <td width=2100px><b>Поставщик</b> <u><?php echo $org->org_name; ?></u>
-
-        </td>
+        <td><b>Поставщик</b> <u><?php echo $org->org_name; ?></u></td>
         <td> (2)</td>
     </tr>
     <tr>
-        <td width=2100px> БИН и адрес местнахождения
-            поставщика <?php echo " <u>" . $org->bin . " " . $org->address; ?></u>
+        <td> БИН и адрес местнахождения поставщика <u><?php echo $org->bin . " " . $org->address; ?></u>
         </td>
         <td> (2а)</td>
     </tr>
     <tr>
-        <td width=2100px> Расчетный счет поставщика <u><?php echo $org->IIK; ?> КБЕ-<?php echo $org->Bank_kbe; ?>
+        <td> Расчетный счет поставщика <u><?php echo $org->IIK; ?> КБЕ-<?php echo $org->Bank_kbe; ?>
                 КНП-<?php echo $org->bank_knp; ?> <?php echo $org->bank_name; ?>
                 БИК <?php echo $org->bank_bik; ?></u></td>
         <td> (2б)</td>
     </tr>
     <tr>
-        <td width=2100px> Свидетельство о постановке на регистрационный учет по
+        <td> Свидетельство о постановке на регистрационный учет по
             НДС <?php echo $org->svidetelstvo_nds; ?> </td>
         <td></td>
     </tr>
     <tr>
-        <td width=2100px> Договор(контракт) на поставку товаров (работ, услуг)
-            №<u><?php echo $firm->dogovor . "        </u> от <u>       " . datetostring($firm->dogovor_date); ?>                 </u>
+        <td> Договор(контракт) на поставку товаров (работ, услуг)
+            №<u><?php echo $firm->dogovor . " </u> от <u> " . datetostring($firm->dogovor_date); ?></u>
         </td>
         <td> (3)</td>
     </tr>
     <tr>
-        <td width=2100px> Условия оплаты по договору
-            (контракту)<u><?php echo $firm->edit1; ?>                                                                                                                             </u>
-        </td>
+        <td> Условия оплаты по договору (контракту) <u><?php echo $firm->edit1; ?></u></td>
         <td> (3а)</td>
     </tr>
     <tr>
-        <td width=2100px> Пункт назначения поставляемых товаров (работ,
+        <td> Пункт назначения поставляемых товаров (работ,
             услуг)<u><?php echo $firm->edit2; ?>                                                                                                                                           </u>
         </td>
         <td> (4)</td>
     </tr>
     <tr align="right">
-        <td width=2100px><i><FONT size=1>(государство, регион, область, город, район) </font> </i></td>
+        <td><i>(государство, регион, область, город, район)</i></td>
         <td></td>
     </tr>
     <tr>
-        <td width=2100px> Поставка товаров осуществлена по доверенности
-            №<u><?php echo $firm->edit3; ?>                                                       </u></td>
+        <td> Поставка товаров осуществлена по доверенности №<u><?php echo $firm->edit3; ?></u></td>
         <td> (5)</td>
     </tr>
     <tr>
-        <td width=2100px> Способ отправления
-            <u><?php echo $firm->edit4; ?>                                                                                                                                                    </u>
-        </td>
+        <td> Способ отправления <u><?php echo $firm->edit4; ?></u></td>
         <td> (6)</td>
     </tr>
     <tr>
-        <td width=2100px> Товарно-транспортная накладная №_______________________ от
-            ________________________________________
-        </td>
+        <td> Товарно-транспортная накладная №_________________ от _____________________________________</td>
         <td> (7)</td>
     </tr>
     <tr>
-        <td width=2100px> Грузоотправитель
-            <u><?php echo $firm->edit5; ?>                                                                                                                     </u>
-        </td>
+        <td> Грузоотправитель <u><?php echo $firm->edit5; ?></u></td>
         <td> (8)</td>
     </tr>
     <tr align="right">
-        <td width=2100px><i><FONT size=1>(РНН, наименование и адрес места нахождения) </font></i></td>
+        <td align="center"><i>(РНН, наименование и адрес места нахождения)</i></td>
         <td></td>
     </tr>
     <tr>
-        <td width=2100px> Грузополучатель
-            <u><?php echo $firm->edit6; ?>                                                                                                                     </u>
-        </td>
+        <td> Грузополучатель <u><?php echo $firm->edit6; ?></u></td>
         <td> (9)</td>
     </tr>
     <tr align="right">
-        <td width=2100px><i><FONT size=1>(РНН, наименование и адрес места нахождения) </font></i></td>
+        <td align="center"><i>(РНН, наименование и адрес места нахождения)</i></td>
         <td></td>
     </tr>
     <tr>
-        <td width=2100px> Получатель <u><?php echo "{$firm->name}"; ?>                </u></td>
-        <td> (10)</td>
+        <td><b>Получатель</b><u><?php echo "{$firm->name}"; ?></u></td>
+        <td>(10)</td>
     </tr>
     <tr>
-        <td width=2100px> РНН, БИН и адрес местонахождения получателя
+        <td> РНН, БИН и адрес местонахождения получателя
             <u><?php echo $firm->rnn . ", " . $firm->bin . " " . $firm->address; ?></u></td>
-        <td> (10а)</td>
+        <td>(10а)</td>
     </tr>
     <tr>
-        <td width=2100px> Расчетный счет получателя <u><?php echo $firm->raschetnyy_schet; ?></u> в банке
+        <td> Расчетный счет получателя <u><?php echo $firm->raschetnyy_schet; ?></u> в банке
             <u><?php echo $bank->name; ?></u></td>
-        <td> (10б)</td>
+        <td>(10б)</td>
     </tr>
 </table>
 <br>
-<table cellSpacing=0 border=1>
+<table class="border-table block">
     <tr>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>№ п/п</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Наименование товаров
-                (работ, услуг)</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Ед. изм.</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Кол-во
-                (объем)</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Цена тенге</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Стоимость товаров
-                (работ, услуг) без НДС</FONT>
-        <td vAlign=center align=middle colSpan=2><FONT size=2>НДС</FONT>
-        <td vAlign=center align=middle rowSpan=2><FONT size=2>Всего стоимость
-                реализации</FONT>
-        <td vAlign=center align=middle colSpan=2><FONT size=2>Акциз</FONT></td>
+        <td align="center" rowspan="2">№ п/п</td>
+        <td align="center" rowspan="2">Наименование товаров(работ, услуг)</td>
+        <td align="center" rowspan="2">Ед. изм.</td>
+        <td align="center" rowspan="2">Кол-во(объем)</td>
+        <td align="center" rowspan="2">Цена тенге</td>
+        <td align="center" rowspan="2">Стоимость товаров(работ, услуг) без НДС</td>
+        <td align="center" colspan="2">НДС</td>
+        <td align="center" rowspan="2">Всего стоимость реализации</td>
+        <td align="center" colspan="2">Акциз</td>
+    </tr>
     <tr>
-        <td vAlign=center align=middle><FONT size=2>Ставка</FONT>
-        <td vAlign=center align=middle><FONT size=2>Сумма</FONT>
-        <td vAlign=center align=middle><FONT size=2>Ставка</FONT>
-        <td vAlign=center align=middle><FONT size=2>Сумма</FONT></td>
+        <td align="center">Ставка</td>
+        <td align="center">Сумма</td>
+        <td align="center">Ставка</td>
+        <td align="center">Сумма</td>
+    </tr>
     <tr>
-        <td vAlign=center align=middle><FONT size=1><I>1</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>2</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>3</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>4</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>5</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>6</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>7</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>8</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>9</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>10</I></FONT>
-        <td vAlign=center align=middle><FONT size=1><I>11</I></FONT></td>
+        <td align="center"><i>1</i></td>
+        <td align="center"><i>2</i></td>
+        <td align="center"><i>3</i></td>
+        <td align="center"><i>4</i></td>
+        <td align="center"><i>5</i></td>
+        <td align="center"><i>6</i></td>
+        <td align="center"><i>7</i></td>
+        <td align="center"><i>8</i></td>
+        <td align="center"><i>9</i></td>
+        <td align="center"><i>10</i></td>
+        <td align="center"><i>11</i></td>
         <?php $sum_bez_nds = 0;
         $sum_nds = 0;
         $sum = 0;
@@ -180,47 +155,46 @@ function f_d2($var)
         $j++):
         if ($tariff_kvt[$j] == 0) continue;
         ?>
+    </tr>
     <tr>
-        <td vAlign=center align=middle><FONT size=1><?php echo $i++; ?> </FONT>
-        <td vAlign=center align=middle><FONT size=1>Электроэнергия</FONT>
-        <td vAlign=center align=middle><FONT size=1>кВт</FONT>
-        <td vAlign=center align=middle><FONT size=1>
-                <?php echo f_d($tariff_kvt[$j]); ?> </FONT>
-        <td vAlign=center align=middle><FONT size=1>
-                <?php echo f_d($tariff_value[$j]); ?> </FONT>
-        <td vAlign=center align=middle><FONT size=1>
-                <?php
-                echo f_d($tariff_kvt[$j] * $tariff_value[$j]);
-                $sum_bez_nds += f_d2($tariff_kvt[$j] * $tariff_value[$j]);
-                ?> </FONT>
-        <td vAlign=center align=middle><FONT size=1> <?php echo f_d($period->nds); ?> </FONT>
-        <td vAlign=center align=middle><FONT size=1>
-                <?php echo f_d($period->nds * $tariff_kvt[$j] * $tariff_value[$j] / 100);
-                $sum_nds += $period->nds * $tariff_kvt[$j] * $tariff_value[$j] / 100; ?>
-            </FONT>
-        <td vAlign=center align=middle><FONT size=1>
-                <?php echo
-                f_d(($period->nds + 100) * $tariff_kvt[$j] * $tariff_value[$j] / 100);
-                $sum += ($period->nds + 100) * $tariff_kvt[$j] * $tariff_value[$j] / 100; ?>
-            </FONT>
-        <td vAlign=center align=middle><FONT size=1>0</FONT>
-        <td vAlign=center align=middle><FONT size=1>0</FONT></td>
+        <td align="center"><?php echo $i++; ?></td>
+        <td align="center">Электроэнергия</td>
+        <td align="center">кВт</td>
+        <td align="center"><?php echo f_d($tariff_kvt[$j]); ?></td>
+        <td align="center"><?php echo f_d($tariff_value[$j]); ?></td>
+        <td align="center">
+            <?php
+            echo f_d($tariff_kvt[$j] * $tariff_value[$j]);
+            $sum_bez_nds += f_d2($tariff_kvt[$j] * $tariff_value[$j]);
+            ?>
+        </td>
+        <td align="center"> <?php echo f_d($period->nds); ?></td>
+        <td align="center">
+            <?php echo f_d($period->nds * $tariff_kvt[$j] * $tariff_value[$j] / 100);
+            $sum_nds += $period->nds * $tariff_kvt[$j] * $tariff_value[$j] / 100; ?>
+        </td>
+        <td align="center">
+            <?php echo
+            f_d(($period->nds + 100) * $tariff_kvt[$j] * $tariff_value[$j] / 100);
+            $sum += ($period->nds + 100) * $tariff_kvt[$j] * $tariff_value[$j] / 100; ?>
+        </td>
+        <td align="center">0</td>
+        <td align="center">0</td>
         <?php endfor; ?>
+    </tr>
     <tr>
-        <td align="left" colSpan=5><FONT size=2><B>Всего по счету:</B></FONT>
-        <td align="right"><FONT size=2><B>&nbsp;<?php echo f_d2($sum_bez_nds); ?></B></FONT>
-        <td align="right" bgColor=#c0c0c0><FONT size=2>&nbsp;</FONT>
-        <td align="right"><FONT size=2><B>&nbsp;<?php echo f_d2($sum_nds); ?></B></FONT>
-        <td align="right"><FONT size=2><B>&nbsp;<?php echo f_d2($sum_bez_nds + $sum_nds); ?></B></FONT>
-        <td align="right" bgColor=#c0c0c0><FONT size=2>&nbsp;</FONT>
-        <td align="right"><FONT size=2><B>&nbsp;</B></FONT></td>
-
+        <td align="left" colspan="5"><b>Всего по счету:</b></td>
+        <td align="right"><b><?php echo f_d2($sum_bez_nds); ?></b></td>
+        <td align="right" class="gray-cube">&nbsp;</td>
+        <td align="right"><b><?php echo f_d2($sum_nds); ?></b></td>
+        <td align="right"><b><?php echo f_d2($sum_bez_nds + $sum_nds); ?></b></td>
+        <td align="right" class="gray-cube">&nbsp;</td>
+        <td align="right"><b></b></td>
+    </tr>
 </table>
-<center>
-    <?php echo num2str(f_d2($sum_bez_nds + $sum_nds)); ?>
-</center>
+<p><?php echo num2str(f_d2($sum_bez_nds + $sum_nds)); ?></p>
 <br>
-<table width=100%>
+<table class="block">
     <tr>
         <td align="left">Руководитель организации</td>
         <td align="right">ВЫДАЛ (Ответственное лицо поставщика)</td>
@@ -240,7 +214,7 @@ function f_d2($var)
 
     <tr>
         <td align="left">Главный бухгалтер организации</td>
-        <td align="right">Бухгалтер потребителя&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+        <td align="right">Бухгалтер потребителя</td>
     </tr>
     <tr>
         <td align="left"><?php echo $org->glav_buh; ?>_______________</td>
@@ -251,8 +225,9 @@ function f_d2($var)
         <td align="right">(Ф.И.О., подпись)</td>
     </tr>
     <tr>
-        <td align="left"><br/>Примечание. Без печати недействительно.<br/>Оригинал (первый экземпляр)-покупателю.<br/>Копия
-            (второй экземпляр)-поставщику.
+        <td align="left"><br/>Примечание. Без печати недействительно.<br/>
+            Оригинал (первый экземпляр)-покупателю.<br/>
+            Копия (второй экземпляр)-поставщику.
         </td>
         <td></td>
     </tr>
