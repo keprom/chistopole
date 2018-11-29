579895528573b88c63169bfcfc988a9a908ea498
keprom <kokshetau.energo.prom@mail.ru>
2018-11-29 16:31:21 +0600

    added notification engine

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -92,6 +92,7 @@ class Billing extends Controller
         $data['poisk'] = $this->session->userdata('poisk');
         if ($this->session->userdata('poisk') == NULL) $data['poisk'] = '1';
         $this->load->view("left", $data);
+		$this->load->view("messages");		
     }
 
     function phpinfo()
@@ -2441,6 +2442,8 @@ class Billing extends Controller
     function perehod()
     {
         $this->db->query("select * from industry.goto_next_period_fine()");
+		$array = array(1 => 'Переход в следующий месяц прошел успешно!');
+		$this->session->set_flashdata('success', $array);		
         redirect("billing");
     }
 
